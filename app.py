ea2a0322030f4e4aba79ab5ecb8ba776bb5e4198
jiaqiwu66 <you@example.com>
2024-04-02 16:38:17 -0700

    first chart push

--- app.py
+++ app.py
@@ -3,15 +3,38 @@ import pandas as pd
 import seaborn as sns
 
 st.set_page_config(
-    page_title="Penguins Explorer",
-    page_icon="🐧",
+    page_title="Iris Explorer",
+    page_icon="🌷",
     layout="centered", # centered, wide 
     initial_sidebar_state="auto", 
     menu_items=None
 )
 
-st.title("🐧 Penguins Explorer")
+st.title("🌷 Iris Explorer")
 
-df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
+df = pd.read_csv('https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv')
 
-st.write(df)
+with st.sidebar:
+    # Input filter options
+    sepal_length_slider = st.slider(
+        "Sepal Length (mm)",
+        min(df["sepal_length"]),
+        max(df["sepal_length"]),
+    )
+
+
+    species_filter = st.multiselect("Species", df["species"].unique())
+
+# Filter data
+
+if species_filter:
+    df = df[df["species"].isin(species_filter)]
+df = df[df["sepal_length"] > sepal_length_slider]
+
+
+# Filter data
+with st.expander("RAW Data"):
+    st.write(df)
+
+
+# st.write(df)
